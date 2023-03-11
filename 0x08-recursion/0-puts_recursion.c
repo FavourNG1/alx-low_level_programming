d78feb9558246d2eb2d2b4650a3f42f335a323d3
FavourNG1 <favournnenna55@gmail.com>
2023-03-11 10:42:31 -0800

    #include "main.h"

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,19 @@
 #include "main.h"
+
 /**
- * _puts_recursion - prints a string, followed by a new line.
+ * _puts_recursion - Print a string
  * @s: string
- * Return: no return.
  */
+
 void _puts_recursion(char *s)
-	
-	if (*s != '\0')
+{
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
 	}
 	else
-	
-	_putchar('\n');
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+}
